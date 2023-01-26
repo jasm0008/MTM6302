2410c239c6b2270a407ba8ce15f56bb94219426a
jasm0008 <jasm0008@algonquinlive.com>
2023-01-26 14:41:27 -0500

    updated

--- week-3/main.js
+++ week-3/main.js
@@ -38,43 +38,55 @@ console.log( $main )
 
 //Select element by any CSS selector
 const $canadaCategory = document.querySelector('nav a:nth-child(2)')
-console.log( $canadaCategory  )
+console.log( $canadaCategory )
 
 const $categories = document.querySelectorAll('nav a')
 console.log( $categories )
+
 //practice CSS selectors
-// console.log(document.querySelectorAll('nav a:not(nav a:last-child)'))
+console.log( document.querySelectorAll('[href^="https"]') )
+console.log( document.querySelectorAll('[href^="cdn"]') )
 
 /*** Traversing the DOM  ***/
-// console.log( $main.children  )
-// console.log( $main.childNodes )
-// console.log( $main.firstElementChild  )
-// console.log( $main.nextElementSibling )
+console.log( $main.children )
+console.log( $main.childNodes )
+console.log( $main.firstElementChild ) 
+console.log( $main.nextElementSibling )
 
 /*** Manipulating attributes */
 //getAttribute()
 //Obtain the id of the main element
+console.log( $main.getAttribute('id') )
+console.log( $main.id)
 
 //exception calling attributes as properties
-// console.log( $main.className )
-// console.log( $main.style);
-// console.log( $main.getAttribute('style'));
-// console.log(typeof $main.getAttribute('style'));
+console.log( $main.className )
+console.log( $main.style);
+console.log( $main.getAttribute('style'));
+console.log(typeof $main.getAttribute('style'));
 
 //setAttribute
 //create the id jumbotron; first div element of main
+$main.firstElementChild.setAttribute('id','jumbotron')
+const $jumbotron = document.getElementById('jumbotron')
 
 //Add  'text-bg-dark' class to the element with id jumbotron; warning!
+// $jumbotron.setAttribute('class', 'text-bg-dark')
+// $jumbotron.setAttribute()
 
 //Add the href attribute to the 'continue reading...' link from jumbotron; https://imdac.github.io/mtm6302/
-// element.setAttribute('target', '_blank');
-// console.log(element.getAttribute('class')); // text-bg-dark
-// console.log(element.getAttribute('href')); // https://imdac.github.io/mtm6302/
-// console.log(element.getAttribute('target')); // _blank
+const $jumbotronLink = document.querySelector('main a')
+console.log($jumbotronLink)
+$jumbotronLink.setAttribute('href','https://imdac.github.io/mtm6302/')
+
+element.setAttribute('target', '_blank');
+console.log(element.getAttribute('class')); // text-white fw-bold
+console.log(element.getAttribute('href')); // https://imdac.github.io/mtm6302/
+console.log(element.getAttribute('target')); // _blank
 
 //Remove
 //Remove the attribute 'target' from the 'continue reading...' link
-// console.log(element.getAttribute('target')); // null 
+console.log(element.getAttribute('target')); // null 
 
 
 
