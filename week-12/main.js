93736719e4f98f9c28bd69ea9888cd24211b3fde
jasm0008 <jasm0008@algonquinlive.com>
2023-03-30 13:42:36 -0400

    updated

--- week-12/main.js
+++ week-12/main.js
@@ -9,15 +9,25 @@ const $users = document.getElementById('user-list')
 
 
 async function fetchData () {
-    const response = await fetch('https://jsonplaceholder.typicode.com/users')
+    const response = await fetch('https://pokeapi.co/api/v2/pokemon/?offset=2&limit=2')
     const json = await response.json()
-    displayUsers(json)
+    displayUsers(json.results)
   }
+
+  fetchData()
+
+// function displayUsers(pokemons){
+//     const htmlTemplate = []
+//     for(const pokemon of pokemons){
+//         htmlTemplate.push(`<li>${pokemon.name}</li>`)
+//     }
+//     $users.innerHTML = htmlTemplate.join('')
+// }
 
 function displayUsers(users){
     const htmlTemplate = []
-    for(const user of json){
-        htmlTemplate.push(`<li>${user[user.name]}</li>`)
+    for(const user of users){
+        htmlTemplate.push(`<li>${user.name}</li>`)
     }
     $users.innerHTML = htmlTemplate.join('')
 }
